7fd6bdd621e9151e2304ec5beebbfdb1528e6258
leavism <ghuydang@gmail.com>
2022-04-19 14:57:04 -0700

    Impletement help command

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const config = require('./config.js');
 const { SapphireClient } = require('@sapphire/framework');
+const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
 
 const client = new SapphireClient(
     { defaultPrefix: config.prefix,
         intents: ['GUILDS', 'GUILD_MESSAGES']}
 );
+
+client.buildHelp = async function () {
+    const help = {}
+    const allCommands = this.stores.get('commands')
+
+    await Promise.all(allCommands.map((command) => {
+        if (!has(help, command.category)) help[command.category] = []
+        help[command.category].push(command)
+    }))
+   return help
+}
+
+String.prototype.titleCase = function () {
+    return this.replace(
+        /\w\S*/g,
+        function (txt) {
+          return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+        }
+    );
+}
 
 client.login(config.token);
---
+++ src/listeners/helpInteraction.js
@@ -0,0 +1,36 @@
+const { Listener } = require('@sapphire/framework');
+const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
+
+class helpInteractionListener extends Listener {
+    constructor(context, options = {}) {
+        super(context, {
+            ...options,
+            event: 'interactionCreate',
+            once: false
+        });
+    }
+
+    async run(interaction) {
+        const help = await this.container.client.buildHelp()
+        if (!interaction.isButton()) return;
+
+        const content = await this.buildCategory(help, interaction.customId)
+        return await interaction.reply({content: content.join(''), split: '\u200b'})
+
+    }
+
+    async buildCategory (helpObj, category) {
+        const helpMessage = []
+        helpMessage.push(`${category.titleCase()} Commands:`, '```asciidoc\n')
+        
+        const commands = helpObj[category]
+        for (let commandIndex = 0; commandIndex < commands.length; commandIndex++){
+            helpMessage.push(`• ${this.container.client.fetchPrefix()}${commands[commandIndex].name} :: ${commands[commandIndex].description}\n`)
+        }
+
+        helpMessage.push('```')
+        return helpMessage
+    }
+}
+
+exports.helpInteractionListener = helpInteractionListener;
