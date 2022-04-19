f0ea39d67bd66c53a8bb616bdb77c5092ecb61f4
leavism <ghuydang@gmail.com>
2022-04-19 14:57:51 -0700

    Renamed for clarity

---
+++ src/listeners/readyEvent.js
@@ -0,0 +1,17 @@
+const { Listener } = require('@sapphire/framework');
+
+class ReadyEventListener extends Listener {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            once: true,
+            event: 'ready'
+        });
+    }
+    run(client) {
+        const { username, id } = client.user;
+        this.container.logger.info(`Successfully logged in as ${username} (${id})`);
+    }
+}
+
+exports.ReadyEventListener = ReadyEventListener;
