f0bbbd397be5b885e9fe803f6d1a8d1844d8b6cb
leavism <ghuydang@gmail.com>
2022-05-25 13:25:32 -0700

    Getting scheduled-task plugin set up

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const config = require('./config.js');
 const { SapphireClient } = require('@sapphire/framework');
-const aws = require('aws-sdk');
+const { ScheduledTaskRedisStrategy } = require('@sapphire/plugin-scheduled-tasks/register-redis');
 const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
 
-const client = new SapphireClient(
-    { 
-        intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'],
-        loadMessageCommandListeners: true
+const options =     { 
+    intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'],
+    loadMessageCommandListeners: true,
+    tasks: {
+        // Using bull (redis)
+        strategy: new ScheduledTaskRedisStrategy({
+            /* You can add your Bull options here, for example we can configure custom Redis connection options: */
+            bull: {
+                redis: {
+                    port: 6379, // Defaults to 6379, but if your Redis server runs on another port configure it here
+                    password: '', // If your Redis server requires a password configure it here
+                    host: 'localhost', // The host at which the redis server is found
+                    db: 2 // Redis database number, defaults to 0 but can be any value between 0 and 15
+                }
+            }
+        })
     }
-);
+};
+
+const client = new SapphireClient(options);
 
 client.buildHelp = async function () {
     const help = {};
