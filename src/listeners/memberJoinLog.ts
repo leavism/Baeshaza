98817394d39b0a849368ee9c621c88a2fe05c54b
leavism <ghuydang@gmail.com>
2023-01-19 18:58:22 +0700

    Basic, common even logging

---
+++ src/listeners/memberJoinLog.ts
@@ -0,0 +1,40 @@
+import { EmbedBuilder } from '@discordjs/builders';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener, ListenerOptions } from '@sapphire/framework';
+import { GuildMember } from 'discord.js';
+import { findTextChannel } from '../lib/utils';
+
+@ApplyOptions<ListenerOptions>({})
+export class MemberJoinLog extends Listener {
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, {
+			...options,
+			event: 'guildMemberAdd',
+		});
+	}
+
+	async constructJoinEmbed(member: GuildMember) {
+		const createdDaysAgo = `(${Math.round((new Date().valueOf() - member.user.createdAt.valueOf()) / (24 * 60 * 60 * 1000))} days ago)`;
+
+		return new EmbedBuilder()
+			.setTitle(`${member.user.tag} has joined the ${member.guild.name} Discord`)
+			.setThumbnail(member.user.displayAvatarURL())
+			.setDescription(`${member.user}`)
+			.setColor([29, 245, 87])
+			.addFields(
+				{ name: 'Joined Discord on', value: `${member.user.createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}\n${createdDaysAgo}`, inline: true },
+				{ name: 'Joined Hearthome on', value: `${new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}`, inline: true },
+			)
+			.setFooter({
+				text: `User ID: ${member.user.id}`,
+			});
+	}
+
+	public async run(member: GuildMember) {
+		const modLogChannel = await findTextChannel(member.guild, 'mod-log');
+
+		return modLogChannel?.send({
+			embeds: [await this.constructJoinEmbed(member)],
+		});
+	}
+}
---
+++ src/listeners/memberLeaveLog.ts
@@ -0,0 +1,40 @@
+import { EmbedBuilder } from '@discordjs/builders';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener, ListenerOptions } from '@sapphire/framework';
+import { GuildMember } from 'discord.js';
+import { findTextChannel } from '../lib/utils';
+
+@ApplyOptions<ListenerOptions>({})
+export class MemberLeaveLog extends Listener {
+	public constructor(context: Listener.Context, options: Listener.Options) {
+		super(context, {
+			...options,
+			event: 'guildMemberRemove',
+		});
+	}
+
+	async constructJoinEmbed(member: GuildMember) {
+		const createdDaysAgo = `(${Math.round((new Date().valueOf() - member.user.createdAt.valueOf()) / (24 * 60 * 60 * 1000))} days ago)`;
+
+		return new EmbedBuilder()
+			.setTitle(`${member.user.tag} has left the ${member.guild.name} Discord`)
+			.setThumbnail(member.user.displayAvatarURL())
+			.setDescription(`${member.user}`)
+			.setColor([235, 64, 52])
+			.addFields(
+				{ name: 'Joined Discord on', value: `${member.user.createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}\n${createdDaysAgo}`, inline: true },
+				{ name: 'Left Hearthome on', value: `${new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}`, inline: true },
+			)
+			.setFooter({
+				text: `User ID: ${member.user.id}`,
+			});
+	}
+
+	public async run(member: GuildMember) {
+		const modLogChannel = await findTextChannel(member.guild, 'mod-log');
+
+		return modLogChannel?.send({
+			embeds: [await this.constructJoinEmbed(member)],
+		});
+	}
+}
