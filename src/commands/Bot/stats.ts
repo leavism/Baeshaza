1991485fa5792ffc3ab6931659a1d1db18e9abaf
leavism <ghuydang@gmail.com>
2023-01-19 14:25:26 +0700

    Added more stats

--- src/commands/Bot/stats.ts
+++ src/commands/Bot/stats.ts
@@ -1,6 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command } from '@sapphire/framework';
-import { EmbedBuilder } from 'discord.js';
+import { DurationFormatter } from '@sapphire/time-utilities';
+import { Command, version as sapphireVersion } from '@sapphire/framework';
+import { EmbedBuilder, codeBlock, version as discordVersion } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
 	description: 'Gets info and stats for the Discord bot.',
@@ -19,7 +20,13 @@ export class UserCommand extends Command {
 			.setColor(0x04ff70)
 			.setThumbnail(`${this.container.client.user?.avatarURL()}`)
 			.addFields(
-				{ name: '🍽️ Serving', value: `${this.container.client.guilds.cache.size} server(s)\n${this.container.client.users.cache.size} user(s)` }
+				{ name: '🍽️ Serving', value: codeBlock(`${this.container.client.guilds.cache.size} server(s)\n${this.container.client.users.cache.size} user(s)`), inline: true },
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				{ name: '🕑 Uptime', value: codeBlock(`${new DurationFormatter().format(this.container.client.uptime!)}`), inline: true },
+				{ name: 'GitHub', value: '[Repository](https://github.com/leavism/Baeshaza)', inline: false },
+			)
+			.setFooter(
+				{ text: `Built using: Sapphire ${sapphireVersion} | Discord.js ${discordVersion} | Node.js ${process.version}` }
 			);
 	}
 
